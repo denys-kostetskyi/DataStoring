ed328ee292c4476434b893150fb807f32daa183a
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-09-03 12:55:03 +0300

    Testing Shared Preferences User repository

--- app/src/main/java/com/denyskostetskyi/datastoring/MainActivity.kt
+++ app/src/main/java/com/denyskostetskyi/datastoring/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.denyskostetskyi.datastoring
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.denyskostetskyi.datastoring.model.User
+import com.denyskostetskyi.datastoring.preferences.SharedPreferencesUserRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        testRepositories()
+    }
+
+    private fun testRepositories() {
+        val user = User(id = 1, firstName = "Denys", lastName = "Kostetskyi")
+        testSharedPreferencesRepository(user)
+    }
+
+    private fun testSharedPreferencesRepository(user: User) {
+        val preferences = getPreferences(Context.MODE_PRIVATE)
+        val repository = SharedPreferencesUserRepository(preferences)
+        repository.saveUser(user)
+        Log.d(TAG_SHARED_PREFERENCES, "Saved User: ${repository.getUser()}")
+        repository.deleteUser()
+        Log.d(TAG_SHARED_PREFERENCES, "User deleted: ${repository.getUser() == User.DEFAULT}")
+    }
+
+    companion object {
+        private const val TAG_SHARED_PREFERENCES = "SharedPreferences"
     }
 }
