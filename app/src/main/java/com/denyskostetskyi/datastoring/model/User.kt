0d891240ec13ab9d547b3e6c694565376d0a1b60
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-09-03 11:48:33 +0300

    Added User model

---
+++ app/src/main/java/com/denyskostetskyi/datastoring/model/User.kt
@@ -0,0 +1,15 @@
+package com.denyskostetskyi.datastoring.model
+
+data class User(
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+) {
+    companion object {
+        val DEFAULT = User(
+            id = 1,
+            firstName = "John",
+            lastName = "Doe"
+        )
+    }
+}
