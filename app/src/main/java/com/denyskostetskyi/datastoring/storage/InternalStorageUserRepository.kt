ee9157b31961107fd73eab189d6bb2063c7ce57b
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-09-05 15:21:25 +0300

    InternalStorageUserRepository improvements

--- app/src/main/java/com/denyskostetskyi/datastoring/storage/InternalStorageUserRepository.kt
+++ app/src/main/java/com/denyskostetskyi/datastoring/storage/InternalStorageUserRepository.kt
@@ -4,18 +4,36 @@ import android.content.Context
 import com.denyskostetskyi.datastoring.model.User
 import java.io.File
 
-class InternalStorageUserRepository(private val applicationContext: Context) {
-    fun saveUser(user: User) {
-        val file = getUserFile()
-        file.writeText("${user.id}$DELIMITER${user.firstName}$DELIMITER${user.lastName}")
-    }
+class InternalStorageUserRepository(context: Context) {
+    private val applicationContext = context.applicationContext
+
+    fun saveUser(user: User) = getUserFile().writeText(userToString(user))
 
     fun getUser(): User {
         val file = getUserFile()
         if (!file.exists()) {
             return User.DEFAULT
         }
-        val data = file.readText().split(DELIMITER)
+        val text = file.readText()
+        return parseUser(text)
+    }
+
+    fun updateUser(user: User) = saveUser(user)
+
+    fun deleteUser() = getUserFile().delete()
+
+    private fun getUserFile() = File(applicationContext.filesDir, FILE_NAME)
+
+    private fun userToString(user: User) = buildString {
+        append(user.id)
+        append(DELIMITER)
+        append(user.firstName)
+        append(DELIMITER)
+        append(user.lastName)
+    }
+
+    private fun parseUser(text: String): User {
+        val data = text.split(DELIMITER)
         return if (data.size == 3) {
             val id = data[0].toIntOrNull() ?: return User.DEFAULT
             User(id, data[1], data[2])
@@ -23,19 +41,6 @@ class InternalStorageUserRepository(private val applicationContext: Context) {
             User.DEFAULT
         }
     }
-
-    fun updateUser(user: User) {
-        saveUser(user)
-    }
-
-    fun deleteUser() {
-        val file = getUserFile()
-        if (file.exists()) {
-            file.delete()
-        }
-    }
-
-    private fun getUserFile(): File = File(applicationContext.filesDir, FILE_NAME)
 
     companion object {
         private const val FILE_NAME = "user.txt"
