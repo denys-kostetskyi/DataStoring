2e9b0a176eb026af53b3065b792085daf197733f
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-09-05 13:44:39 +0300

    Added Room user repository

--- app/src/main/java/com/denyskostetskyi/datastoring/room/UserDao.kt
+++ app/src/main/java/com/denyskostetskyi/datastoring/room/UserDao.kt
@@ -5,15 +5,11 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
-    @Query("SELECT * FROM users")
-    fun getAllUsers(): Flow<List<UserDbModel>>
-
     @Query("SELECT * FROM users WHERE id = :id")
-    fun getUser(id: Int): Flow<UserDbModel>
+    suspend fun getUser(id: Int): UserDbModel
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveUser(user: UserDbModel)
